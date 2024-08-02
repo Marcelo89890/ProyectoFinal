2dcc8e29da2ac6856a2adeb55c2d8f5d62b0becf
Marcelo89890 <marcelo.morap19@gmail.com>
2024-08-01 19:52:38 -0600

    Crear Elementos HTML JS resolucion

--- js/Main.js
+++ js/Main.js
@@ -7,11 +7,11 @@ document.addEventListener('DOMContentLoaded', ev => {
 
 ///0-
 function MostrarHTML() {
-
+    //
     let navbar = document.getElementById("Navbar").innerHTML= CrearNavbar();
-    // let footer;
-    
-    // let Noticias;
+    let footer= document.getElementById("Footer").innerHTML= CrearFooter();
+
+    let Noticias = document.getElementById("Noticias").innerHTML= CrearTarjetaNoticias();
     
 }
 
