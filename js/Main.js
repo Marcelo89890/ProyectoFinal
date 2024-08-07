3e75d2c475878739d94e21b736339d00d6c4132d
Marcelo89890 <marcelo.morap19@gmail.com>
2024-08-06 21:24:30 -0600

    Solucion Card con API Rick and Mortyn

--- js/Main.js
+++ js/Main.js
@@ -1,10 +1,12 @@
 import { MostrarHTML } from "/js/Global.js"
+import { MostrarCard } from "/js/Card.js"
 import {ObtenerDatosConfiguracion, ObtenerDatosApiRick} from "/js/Servicios_Config.js"
 ///Componentes ->
 document.addEventListener('DOMContentLoaded', ev => {
     MostrarHTML()
     ObtenerDatosConfiguracion()
     ObtenerDatosApiRick()
+    MostrarCard()
 
 })
 
