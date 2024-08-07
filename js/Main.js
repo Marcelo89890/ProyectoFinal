932e5ca6f0195a82dc7a0aed4919b8c91eae4ba8
Marcelo89890 <marcelo.morap19@gmail.com>
2024-08-06 20:58:19 -0600

    Navbar, Footer Dinamico API

--- js/Main.js
+++ js/Main.js
@@ -1,9 +1,10 @@
 import { MostrarHTML } from "/js/Global.js"
-import {ObtenerDatosConfiguracion} from "/js/Servicios_Config.js"
+import {ObtenerDatosConfiguracion, ObtenerDatosApiRick} from "/js/Servicios_Config.js"
 ///Componentes ->
 document.addEventListener('DOMContentLoaded', ev => {
     MostrarHTML()
     ObtenerDatosConfiguracion()
+    ObtenerDatosApiRick()
 
 })
 
--- js/Servicios_Config.js
+++ js/Servicios_Config.js
@@ -10,6 +10,32 @@ export  const ObtenerDatosConfiguracion = async () => {
             const data = await response.json();
 
             console.table(data);
+            return data
+    
+        }else{
+            console.log("Error al consultar el API"+response.status)
+        }
+       
+    } catch (error) {
+
+        console.log("Error en la red"+ error);
+
+    }
+
+};
+
+export  const ObtenerDatosApiRick = async () => {
+
+    const url = "https://rickandmortyapi.com/api/character"
+
+    try {
+
+        const response = await fetch(url);
+        if (response.status == "200") {
+            const data = await response.json();
+
+            console.warn(data.results);
+            return data.results
     
         }else{
             console.log("Error al consultar el API"+response.status)
